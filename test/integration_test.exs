05cb563f26d008ad807a478f399fb42fd6643331
Zach Allaun <zach.allaun@gmail.com>
2023-01-20 13:51:21 -0500

    test: n to new assertion

--- test/integration_test.exs
+++ test/integration_test.exs
@@ -2,3 +2,4 @@ import Mneme.Integration
 
 integration_test("literals_test", "literals_test.1")
 integration_test("literals_test", "literals_test.2")
+integration_test("literals_test", "literals_test.3")
