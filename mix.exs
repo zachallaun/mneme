80f1e9b563e4a948493511472e39ede41131691a
Zach Allaun <zach.allaun@gmail.com>
2023-02-21 11:17:44 -0500

    chore: fix required elixir version and remove unused dep

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Mneme.MixProject do
     [
       app: @app,
       version: version(),
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
