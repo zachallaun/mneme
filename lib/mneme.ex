48c8d2e8d8512c019c8487eb122706f8da0fdf7f
Zach Allaun <zach.allaun@gmail.com>
2023-02-21 11:25:37 -0500

    docs: add badges

--- lib/mneme.ex
+++ lib/mneme.ex
@@ -1,6 +1,8 @@
 defmodule Mneme do
   @external_resource "mix.exs"
   @moduledoc """
+  [![Hex.pm](https://img.shields.io/hexpm/v/mneme.svg)](https://hex.pm/packages/mneme) [![Docs](https://img.shields.io/badge/hexdocs-docs-8e7ce6.svg)](https://hexdocs.pm/mneme) [![CI](https://github.com/zachallaun/mneme/actions/workflows/ci.yml/badge.svg)](https://github.com/zachallaun/mneme/actions/workflows/ci.yml)
+
   /ni:mi:/ - Snapshot testing integrated into ExUnit.
 
   Snapshot tests assert that some expression matches a reference value.
