b0ab008e94dc43600845bde03e17c7e941b0f76c
Zach Allaun <zach.allaun@gmail.com>
2023-02-21 08:54:18 -0500

    test: basic test for match failure

--- test/mneme_test.exs
+++ test/mneme_test.exs
@@ -9,5 +9,15 @@ defmodule MnemeTest do
         auto_assert :foo
       end
     end
+
+    @mneme action: :reject
+    test "raises when the match fails" do
+      error =
+        assert_raise ExUnit.AssertionError, fn ->
+          auto_assert :foo <- :bar
+        end
+
+      assert %{left: :foo, right: :bar, message: "match (=) failed"} = error
+    end
   end
 end
