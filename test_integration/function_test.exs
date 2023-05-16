3d1fb22584899f9f40237ef7f3487943e32e387e
Zach Allaun <zach.allaun@gmail.com>
2023-05-16 09:12:15 -0400

    test: only test function guard serialization in >= 1.14.4

---
+++ test_integration/function_test.exs
@@ -0,0 +1,18 @@
+# version: >= 1.14.4
+defmodule Mneme.Integration.FunctionTest do
+  use ExUnit.Case
+  use Mneme
+
+  test "functions" do
+    # y
+    auto_assert fun when is_function(fun, 1) <- &Function.identity/1
+
+    fun = &Function.identity/1
+
+    # y
+    auto_assert ^fun <- fun
+
+    # y
+    auto_assert fun when is_function(fun, 2) <- &Enum.map/2
+  end
+end
