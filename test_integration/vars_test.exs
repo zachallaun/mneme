af9823544371fc4ced1ac52752e3574cd63c5785
Zach Allaun <zach.allaun@gmail.com>
2023-05-17 13:42:25 -0400

    test: compat with elixir 1.14.3

--- test_integration/vars_test.exs
+++ test_integration/vars_test.exs
@@ -18,7 +18,6 @@ defmodule Mneme.Integration.VarsTest do
     auto_assert ref1 when is_reference(ref1) <- make_ref()
 
     pid = self()
-
     # y
     auto_assert ^pid <- self()
     # k y
@@ -30,10 +29,8 @@ defmodule Mneme.Integration.VarsTest do
   test "re-use var with single guard if same value appears multiple times" do
     # y
     auto_assert [pid, pid] when is_pid(pid) <- [self(), self()]
-
     # y
     auto_assert [pid, [pid, [pid]]] when is_pid(pid) <- [self(), [self(), [self()]]]
-
     # k y
     auto_assert [pid, %{p: pid, r: ref}] when is_pid(pid) and is_reference(ref) <-
                   [
