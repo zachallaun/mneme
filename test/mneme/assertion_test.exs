2f54c0d914658db96f8e37ca50e2e47c3d1ab970
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2024-05-30 15:03:12 +0200

    Fixed tests for 1.14/1.15

--- test/mneme/assertion_test.exs
+++ test/mneme/assertion_test.exs
@@ -91,8 +91,11 @@ defmodule Mneme.AssertionTest do
       auto_assert [2, 3] <- foo
       auto_assert <<foo::binary-size(3), _::binary>> <- "abc def"
       auto_assert "abc" <- foo
-      auto_assert ^foo <> " " <> foo <- "abc def"
-      auto_assert "def" <- foo
+
+      if Version.compare(System.version(), "1.16.0") in [:gt, :eq] do
+        auto_assert ^foo <> " " <> foo <- "abc def"
+        auto_assert "def" <- foo
+      end
 
       pinned = [3]
       auto_assert list when is_list(list) and length(list) == 3 <- [1, 2, 3]
