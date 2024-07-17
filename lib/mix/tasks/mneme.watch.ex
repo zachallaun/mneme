08b9790d11141cb4285eccdf19a737e670fc517d
Zach Allaun <zach.allaun@gmail.com>
2024-07-17 13:02:16 -0400

    chore: remove os restriction on mix mneme.watch

--- lib/mix/tasks/mneme.watch.ex
+++ lib/mix/tasks/mneme.watch.ex
@@ -28,7 +28,6 @@ defmodule Mix.Tasks.Mneme.Watch do
   @spec run([String.t()]) :: no_return()
   def run(args) do
     Mix.env(:test)
-    ensure_os!()
 
     children = [
       {Mneme.Watch.TestRunner, cli_args: args}
@@ -37,26 +36,5 @@ defmodule Mix.Tasks.Mneme.Watch do
     Supervisor.start_link(children, strategy: :one_for_one)
 
     :timer.sleep(:infinity)
-  end
-
-  defp ensure_os! do
-    case os_type() do
-      :unix ->
-        :ok
-
-      unsupported ->
-        error = "file watcher is unsupported on OS: #{inspect(unsupported)}"
-
-        [:red, "error: ", :default_color, error]
-        |> IO.ANSI.format()
-        |> then(&IO.puts(:stderr, &1))
-
-        System.halt(1)
-    end
-  end
-
-  defp os_type do
-    {os_type, _} = :os.type()
-    os_type
   end
 end
