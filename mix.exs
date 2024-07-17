a54609f17ff20f94f95364f006142f1fe4f7c5df
Zach Allaun <zach.allaun@gmail.com>
2024-07-17 13:03:01 -0400

    chore: bump version to 0.9.0-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mneme.MixProject do
   @app :mneme
   @source_url "https://github.com/zachallaun/mneme"
 
-  def version, do: "0.8.2"
+  def version, do: "0.9.0-dev"
 
   def project do
     [
