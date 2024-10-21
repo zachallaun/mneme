45333e7e5ead1a02454d92439c4f395f01d94059
Zach Allaun <zach.allaun@gmail.com>
2024-10-21 10:47:48 -0400

    test: increase default assert_receive_timeout if CI=true

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,11 @@
-ExUnit.start(exclude: [mneme_not_started: true])
+ci? = System.get_env("CI") == "true"
+
+ex_unit_config = [
+  exclude: [mneme_not_started: true],
+  assert_receive_timeout: if(ci?, do: 1_000, else: 100)
+]
+
+ExUnit.start(ex_unit_config)
 
 if System.get_env("START_MNEME") != "false" do
   Mneme.start()
