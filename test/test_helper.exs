20df3fb6aefde6b6d555d39c58a988e049707d44
Zach Allaun <zach.allaun@gmail.com>
2024-10-29 09:13:14 -0400

    test: increase ci assert_receive_timeout a silly amount

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,7 +2,7 @@ ci? = System.get_env("CI") == "true"
 
 ex_unit_config = [
   exclude: [mneme_not_started: true],
-  assert_receive_timeout: if(ci?, do: 1_000, else: 100)
+  assert_receive_timeout: if(ci?, do: 5_000, else: 100)
 ]
 
 ExUnit.start(ex_unit_config)
